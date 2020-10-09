01216e883b78668a8dc54305ea63f1beb94d6db8
Manuel <manuel.serrano943@gmail.com>
2020-10-10 01:11:02 +0200

    Added external CSS + Update Card, badged with version and text changing by hover added to project

--- index.js
+++ index.js
@@ -2,19 +2,18 @@ function showMessage_FirstBox() {
     
     var x = document.getElementById("reward_FirstBox");
     var y = document.getElementById("close_FirstBox");
-    var z = document.getElementById("rewardContent_FirstBox");
+    var z = document.getElementById("rewardText_FirstBox");
 
     if(x === null && z.textContent === null) {
         z.textContent = "¡You claimed all the rewards!"
     }else if(x.style.display === 'none' || x.style.display === '' || y.style.display === 'none' || y.style.display === '') {
         y.style.display = "block";
-        x.style.display = "block";  
+        x.style.display = "block";
     }
    
 }
 
 function showResult_FirstBox() {
     var x = document.getElementById("messageResult_FirstBox");
-
     x.textContent = "¡You already got this reward!"
 }
