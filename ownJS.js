d25822e83e8f828da1e48f3149e7293ad3f5dfd8
Manuel <manuel.serrano943@gmail.com>
2020-10-10 00:25:23 +0200

    displaying grid, reward alert message with dismissing content using js and bootstrap components

---
+++ ownJS.js
@@ -0,0 +1,20 @@
+function showMessage_FirstBox() {
+    
+    var x = document.getElementById("reward_FirstBox");
+    var y = document.getElementById("close_FirstBox");
+    var z = document.getElementById("rewardContent_FirstBox");
+
+    if(x === null && z.textContent === null) {
+        z.textContent = "¡You claimed all the rewards!"
+    }else if(x.style.display === 'none' || x.style.display === '' || y.style.display === 'none' || y.style.display === '') {
+        y.style.display = "block";
+        x.style.display = "block";  
+    }
+   
+}
+
+function showResult_FirstBox() {
+    var x = document.getElementById("messageResult_FirstBox");
+
+    x.textContent = "¡You already got this reward!"
+}
