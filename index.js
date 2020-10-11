67983f77fefb5c2d1646e4480acef8e4dd53015b
Manuel <manuel.serrano943@gmail.com>
2020-10-11 03:48:33 +0200

    Added basic control to submit email with bootstrap4 component and basic javascript

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 window.addEventListener('DOMContentLoaded', function(){
 
     // FIRST BOX
-
+    var box1 = document.getElementById('box1');
     document.getElementById("get_reward_box1").addEventListener('click', function(){
         var x = document.getElementById("reward_box1");
         var y = document.getElementById("close_box1");
         var z = document.getElementById("messageResult_FirstBox");
 
         ShowAndHideContent(x, y);
-        $('.box1').css("height", "190");
+        box1.setAttribute("style", "height: 190px");
 
         y.addEventListener("click", function() {
             z.textContent = "You've claimed all the rewards available";
-            $('.box1').css("height", "150");
+            box1.setAttribute("style", "height: 150px");
         })
     });
 
@@ -33,7 +33,7 @@ window.addEventListener('DOMContentLoaded', function(){
     });
 
     // THIRD BOX
-    
+
     var box3 = document.getElementById("box3");
     var box3_h3 = document.querySelector("#box3 h3");
 
@@ -48,32 +48,30 @@ window.addEventListener('DOMContentLoaded', function(){
     })
 
 
+    // FOURTH BOX
+
+    document.getElementById("email_Box4").addEventListener('keydown', function() {
+        var e = window.event || e;
+        var key = e.keyCode;
+        //space pressed
+        if (key == 32) { //key 32 is the space
+        e.preventDefault();
+        }
+    })
+    
+    document.getElementById("submit_Box4").addEventListener("click", function(){
+        var email = document.getElementById("email_Box4").value;
+        var domain = document.getElementById("chooseEmailDomain").options[document.getElementById("chooseEmailDomain").selectedIndex].value;
+
+        document.getElementById("emailInfo_Box4").setAttribute("style", "display: block");
+
+        if(email.length < 5) {
+            document.getElementById("emailSent_Box4").innerHTML = "You've to write an email to subscribe"
+        }else {
+            document.getElementById("emailSent_Box4").innerHTML = `You've subscribed with ${email}@${domain} `
+        }  
+        
+    })
+    
+
 });
-    
-    /*$(function(){
-    
-        $('#chooseEmailOption').on('click', '.dropdown-item', function() {
-            alert('AAA');
-            $('#emailOptionButton').text($(this).text());
-        });
-    });*/
-
-
-
-
-/*function showMessage_FirstBox() {
-    
-    var x = document.getElementById("reward_FirstBox");
-    var y = document.getElementById("close_FirstBox");
-    var z = document.getElementById("rewardText_FirstBox");
-
-    if(x === null && z.textContent === null) {
-        z.textContent = "¡You claimed all the rewards!"
-    }else if(x.style.display === 'none' || x.style.display === '' || y.style.display === 'none' || y.style.display === '') {
-        y.style.display = "block";
-        x.style.display = "block";
-        $('.box1').css("height", "190");
-    }
-   
-}*/
-
